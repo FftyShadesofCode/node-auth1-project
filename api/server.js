f8f581b88fdcc9d760c5d524d17bb4a7a92a04d0
FiftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-24 20:58:33 -0500

    killed it

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require('express-session');
-const Store = require('connect-session-knex')(session)
+const Store = require('cookie-session')
 /**
  Do what needs to be done to support sessions with the `express-session` package!
  To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -12,7 +12,7 @@ const Store = require('connect-session-knex')(session)
  Users that do authenticate should have a session persisted on the server,
  and a cookie set on the client. The name of the cookie should be "chocolatechip".
 
- The session can be persisted in memory (would not be adecuate for production)
+ The session can be persisted in memory (would not be adequate for production)
  or you can use a session store like `connect-session-knex`.
  */
 
